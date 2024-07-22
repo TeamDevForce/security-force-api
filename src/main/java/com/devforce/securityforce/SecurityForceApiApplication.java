2abc9f7f246f0771d413638d9ec9200990cd71c1
Marcelo Sena <marcelomarcos2@gmail.com>
2024-07-22 17:37:02 -0300

    DEV-1000 -  [feat]: Commit inicial

---
+++ src/main/java/com/devforce/securityforce/SecurityForceApiApplication.java
@@ -0,0 +1,13 @@
+package com.devforce.securityforce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityForceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityForceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devforce/security_force_api/SecurityForceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devforce.security_force_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityForceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
