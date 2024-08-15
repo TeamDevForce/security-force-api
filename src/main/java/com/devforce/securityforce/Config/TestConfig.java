ce2505d901604a518b67ea5ba92961e43c28d329
Lukas-Rodrigo <lucasrodrigo0503@gmail.com>
2024-08-15 12:26:51 -0300

    SEC-28 - [feat]: Adição da classe 'TestConfig' e mapeada para perfil de teste

---
+++ src/main/java/com/devforce/securityforce/Config/TestConfig.java
@@ -0,0 +1,10 @@
+package com.devforce.securityforce.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+}
