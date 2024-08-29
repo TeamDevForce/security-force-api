d770f33d79788c7e0dc98e7ff1382f960348f60b
Iago Freire <iagojfreire@gmail.com>
2024-08-29 14:57:47 -0300

    SEC-69 - [feat]: Adição da função procurar por ID na camada Service

--- src/main/java/com/devforce/securityforce/services/UsuarioService.java
+++ src/main/java/com/devforce/securityforce/services/UsuarioService.java
@@ -1,9 +1,22 @@
 package com.devforce.securityforce.services;
 
+import com.devforce.securityforce.model.Usuario;
 import com.devforce.securityforce.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
 public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    public Usuario findById(Long id) {
+        Optional<Usuario> usuarioOptional = usuarioRepository.findById(id);
+        if (usuarioOptional.isPresent()) {
+            return usuarioOptional.get();
+        }
+        throw new RuntimeException("Usuário não encontrado");
+    }
 }
