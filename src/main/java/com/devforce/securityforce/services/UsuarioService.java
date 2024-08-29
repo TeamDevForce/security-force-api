3dfd1cd885452d785d5fde0fa4795ddf0200fc06
Iago Freire <iagojfreire@gmail.com>
2024-08-29 14:43:07 -0300

    SEC-65 - [feat]: Implementação da camada Service e injeção de dependência UsuarioRepository

---
+++ src/main/java/com/devforce/securityforce/services/UsuarioService.java
@@ -0,0 +1,9 @@
+package com.devforce.securityforce.services;
+
+import com.devforce.securityforce.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UsuarioService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+}
