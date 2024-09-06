57069869e100c6efcc04dd5f8412c085b48ef521
Lukas-Rodrigo <lucasrodrigo0503@gmail.com>
2024-09-06 11:40:53 -0300

    SEC-78 - [feat]: Operações CRUD - Criar credenciais

--- src/main/java/com/devforce/securityforce/services/exceptions/CredencialService.java
+++ src/main/java/com/devforce/securityforce/services/exceptions/CredencialService.java
@@ -1,8 +1,11 @@
 package com.devforce.securityforce.services.exceptions;
 
+import com.devforce.securityforce.model.Credencial;
 import com.devforce.securityforce.repositories.CredencialRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class CredencialService {
@@ -10,4 +13,8 @@ public class CredencialService {
     @Autowired
     private CredencialRepository credencialRepository;
 
+    public Credencial create(Credencial credencial) {
+        credencial.setDataCriacao(LocalDate.now());
+        return credencialRepository.save(credencial);
+    }
 }
