280a527fe72fd336745324e95e5c5a44ffcb83fb
Lukas-Rodrigo <lucasrodrigo0503@gmail.com>
2024-09-10 12:11:34 -0300

    SEC-84 - [feat]: Operações CRUD - Deletar categoria

---
+++ src/main/java/com/devforce/securityforce/services/exceptions/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.devforce.securityforce.services.exceptions;
+
+import com.devforce.securityforce.model.Categoria;
+import com.devforce.securityforce.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public void delete(Long id) {
+        Optional<Categoria> categoriaOptional = categoriaRepository.findById(id);
+        if (categoriaOptional.isPresent()) {
+            categoriaRepository.delete(categoriaOptional.get());
+        } else {
+            throw new ResourceNotFoundException("Categoria não encontrada com o id: " + id);
+        }
+    }
+}
