8894bea289be749b402478421bb3ca7caa57a415
Lukas-Rodrigo <lucasrodrigo0503@gmail.com>
2024-09-18 08:58:00 -0300

    SEC-81 - [feat]: Operações CRUD - Buscar credenciais

--- src/main/java/com/devforce/securityforce/services/CredencialService.java
+++ src/main/java/com/devforce/securityforce/services/CredencialService.java
@@ -2,10 +2,12 @@ package com.devforce.securityforce.services;
 
 import com.devforce.securityforce.model.Credencial;
 import com.devforce.securityforce.repositories.CredencialRepository;
+import com.devforce.securityforce.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Service
 public class CredencialService {
@@ -16,5 +18,13 @@ public class CredencialService {
     public Credencial create(Credencial credencial) {
         credencial.setDataCriacao(LocalDate.now());
         return credencialRepository.save(credencial);
+    }
+
+    public Credencial findById(Long id) {
+        Optional<Credencial> credencialOptional = credencialRepository.findById(id);
+        if (credencialOptional.isPresent()) {
+            return credencialOptional.get();
+        }
+        throw new ResourceNotFoundException("Credencial não encontrada com o id: " + id);
     }
 }
